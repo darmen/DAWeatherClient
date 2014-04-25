c993cda7a787a52fb7c03e81ade4041a4416e78f
Darmen Amanbayev <ioxanssen@gmail.com>
2014-04-25 17:01:45 +0600

    Here we go

---
+++ DAWeatherClient/DAWeatherClient.h
@@ -0,0 +1,13 @@
+//
+//  DAWeatherClient.h
+//  
+//
+//  Created by Darmen Amanbayev on 4/25/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DAWeatherClient : NSObject
+
+@end
---
+++ DAWeatherClient/DAWeatherClient.m
@@ -0,0 +1,13 @@
+//
+//  DAWeatherClient.m
+//  
+//
+//  Created by Darmen Amanbayev on 4/25/14.
+//
+//
+
+#import "DAWeatherClient.h"
+
+@implementation DAWeatherClient
+
+@end
